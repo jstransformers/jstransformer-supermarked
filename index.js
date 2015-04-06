53c7df23d049af0d52c229950cf0b02ce7ffc8e0
Rob Loach <robloach@gmail.com>
2015-04-06 13:56:51 -0400

    feat(inputFormats): Add inputFormats

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var supermarked = require('supermarked');
 
 exports.name = 'supermarked';
 exports.outputFormat = 'html';
+exports.inputFormats = ['supermarked', 'markdown', 'md'];
 exports.render = function (str, options) {
   return supermarked(str, options);
 };
